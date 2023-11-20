a7337be943dbc9fa754ed7649a6c99ccd727ac4c
friendtheend <luyifeng0131@gmail.com>
2023-11-19 21:21:04 -0800

    Initial Commit

---
+++ final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final
+//
+//  Created by 林允儿老公专属Mac on 2023/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by 林允儿老公专属Mac on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
