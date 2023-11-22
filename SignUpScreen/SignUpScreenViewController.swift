f423300c32ada52f5e1f711ce50c9ba2de373389
friendtheend <luyifeng0131@gmail.com>
2023-11-22 00:32:42 -0800

    Add files via upload

---
+++ SignUpScreen/SignUpScreenViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SignUpScreenViewController.swift
+//  final pj
+//
+//  Created by 林允儿老公专属Mac on 2023/11/21.
+//
+
+import UIKit
+//import FirebaseAuth
+//import FirebaseFirestore
+//import FirebaseFirestoreSwift
+
+class SignUpScreenController: UIViewController {
+
+    let SignUpScreen = SignUpScreenView()
+    
+//    var currentUser:FirebaseAuth.User?
+//    
+//    let database = Firestore.firestore()
+//    
+//    let childProgressView = ProgressSpinnerViewController()
+    
+    override func loadView() {
+        view = SignUpScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        SignUpScreen.buttonRegister.addTarget(self, action: #selector(onRegisterTapped), for: .touchUpInside)
+        title = "Register"
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardOnTap))
+        tapRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapRecognizer)
+
+    }
+
+
+    //MARK: Hide Keyboard...
+    @objc func hideKeyboardOnTap(){
+        //MARK: removing the keyboard from screen...
+        view.endEditing(true)
+    }
+    
+    @objc func onRegisterTapped(){
+        //MARK: creating a new user on Firebase...
+        //registerNewAccount()
+    }
+    
+}
+
