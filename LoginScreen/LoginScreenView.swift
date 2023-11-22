02697a73c225e0060b648001da2e438820ea793d
friendtheend <luyifeng0131@gmail.com>
2023-11-21 23:57:03 -0800

    UI

---
+++ LoginScreen/LoginScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreenView.swift
+//  final pj
+//
+//  Created by 林允儿老公专属Mac on 2023/11/21.
+//
+
+import SwiftUI
+
+struct LoginScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreenView()
+}
---
+++ SignUpScreen/SignUpScreenViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpScreenViewController.swift
+//  final pj
+//
+//  Created by 林允儿老公专属Mac on 2023/11/21.
+//
+
+import Foundation
