d956386a529736dec526b21077bd1aae315efe34
friendtheend <luyifeng0131@gmail.com>
2023-11-21 18:29:23 -0800

    Initial Commit

---
+++ final pj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  final pj
+//
+//  Created by 林允儿老公专属Mac on 2023/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
