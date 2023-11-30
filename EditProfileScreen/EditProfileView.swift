37a6cccf9e05975f703ad564573aeecdf6a84890
friendtheend <luyifeng0131@gmail.com>
2023-11-30 14:39:49 -0800

    me页面和edit profile页面

---
+++ EditProfileScreen/EditProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileView.swift
+//  final pj
+//
+//  Created by 林允儿老公专属Mac on 2023/11/30.
+//
+
+import Foundation
---
+++ EditProfileScreen/EditProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileViewController.swift
+//  final pj
+//
+//  Created by 林允儿老公专属Mac on 2023/11/30.
+//
+
+import Foundation
